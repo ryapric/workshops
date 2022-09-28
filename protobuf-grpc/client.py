589ce2f2c4b1aa385c0b3050dde61c5699179fba
Ryan J. Price <ryapric@gmail.com>
2022-09-28 12:06:41 -0500

    Fix Python client code, and doc accordingly

--- protobuf-grpc/client.py
+++ protobuf-grpc/client.py
@@ -1,8 +1,21 @@
 import grpc
+import pb.example_pb2
 import pb.example_pb2_grpc
 
 if __name__ == "__main__":
     with grpc.insecure_channel("127.0.0.1:8080") as channel:
         stub = pb.example_pb2_grpc.ExampleStub(channel)
-        echo_response = stub.Echo("hello grpc")
-        getrecord_response = stub.GetRecord()
+
+        # This is pretty infuriating -- Python protobuf code isn't generated
+        # directly, it generates a *Python* generator to generate the code at
+        # runtime (in *_pb2.py). As such, you don't get completion hints in
+        # editors because there's no data types to autofill etc. So, look at
+        # your proto file and determine how you should be setting up your
+        # requests!
+        echo_request = pb.example_pb2.Echoable(msg = "hello grpc")
+        echo_response = stub.Echo(echo_request)
+        print(echo_response)
+
+        getrecord_request = pb.example_pb2.GetRecordRequest(name = "Thomas Anderson")
+        getrecord_response = stub.GetRecord(getrecord_request)
+        print(getrecord_response)
